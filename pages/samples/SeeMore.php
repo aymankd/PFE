a37db2bb90e8fa556233ab9956885c7fd20db4de
Nabil <bakournabilests@gmail.com>
2020-04-01 19:33:00 +0100

    Rating + Comments

--- pages/samples/SeeMore.php
+++ pages/samples/SeeMore.php
@@ -1120,6 +1120,9 @@ while(($rowEQ=$resEQ->fetch_assoc()) && $eqn<=4 )
       <link rel="stylesheet" href="../../Resourse/cssSm/font-awesome.min.css">
       <!--bootstrap css-->
       <link rel="stylesheet" href="../../Resourse/cssSm/bootstrap.min.css">
+      <link href="../../Resourse/cssSm/ratingBot/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+
       <!--animate css-->
       <link rel="stylesheet" href="../../Resourse/cssSm/animate-wow.css">
       <!--main css-->
@@ -1127,6 +1130,15 @@ while(($rowEQ=$resEQ->fetch_assoc()) && $eqn<=4 )
       <link rel="stylesheet" href="../../Resourse/cssSm/bootstrap-select.min.css">
       <link rel="stylesheet" href="../../Resourse/cssSm/slick.min.css">
       <link rel="stylesheet" href="../../Resourse/cssSm/select2.min.css">
+      <link rel="stylesheet" href="../../Resourse/cssSm/ratingBot/rating.css">
+      <link rel="stylesheet" href="../../Resourse/cssSm/Comments/commt.css">
+      
+
+       <!--comments css-->
+    
+
+
+
       <!--responsive css-->
       <link rel="stylesheet" href="../../Resourse/cssSm/responsive.css">
 
@@ -1296,7 +1308,149 @@ while(($rowEQ=$resEQ->fetch_assoc()) && $eqn<=4 )
                            <?=$regl?>
                            </p>
                         </div>    
-                     
+                        
+                        <div class="spe-a">
+                        <h4>rating d zeb</h4>
+    <div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-md-6">
+            <div class="well well-sm">
+                <div class="row">
+                    <div class="col-xs-12 col-md-6 text-center">
+                        <h1 class="rating-num">
+                            4.0</h1>
+                        <div class="rating">
+                            <span><i class="fas fa-star"></i></span><span><i class="fas fa-star"></i>
+                            </span><span ><i class="fas fa-star"></i></span><span><i class="fas fa-star"></i>
+                            </span><span ><i class="far fa-star"></i></span>
+                        </div>
+                        <div>
+                            <span ><i class="fas fa-user"></i>  </span>1,050,008 total
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-6">
+                        <div class="row rating-desc">
+                            <div class="col-xs-3 col-md-3 text-right">
+                                <span><i class="fas fa-star"></i></span>5
+                            </div>
+                            <div class="col-xs-8 col-md-9">
+                                <div class="progress progress-striped">
+                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
+                                        aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                                        <span class="sr-only">80%</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end 5 -->
+                            <div class="col-xs-3 col-md-3 text-right">
+                                <span ><i class="fas fa-star"></i></span>4
+                            </div>
+                            <div class="col-xs-8 col-md-9">
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
+                                        aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                                        <span class="sr-only">60%</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end 4 -->
+                            <div class="col-xs-3 col-md-3 text-right">
+                                <span ><i class="fas fa-star"></i></span>3
+                            </div>
+                            <div class="col-xs-8 col-md-9">
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20"
+                                        aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                                        <span class="sr-only">40%</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end 3 -->
+                            <div class="col-xs-3 col-md-3 text-right">
+                                <span ><i class="fas fa-star"></i><span>2
+                            </div>
+                            <div class="col-xs-8 col-md-9">
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="20"
+                                        aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                                        <span class="sr-only">20%</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end 2 -->
+                            <div class="col-xs-3 col-md-3 text-right">
+                                <span ><i class="fas fa-star"></i></span>1
+                            </div>
+                            <div class="col-xs-8 col-md-9">
+                                <div class="progress">
+                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80"
+                                        aria-valuemin="0" aria-valuemax="100" style="width: 15%">
+                                        <span class="sr-only">15%</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- end 1 -->
+                        </div>
+                        <!-- end row -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="spe-a">
+ <h4>comments d zeb</h4>
+      
+ <div class="card Comments">
+	    <div class="card-body">
+	        <div class="row">
+        	    <div class="col-md-2">
+        	        <img src="https://image.ibb.co/jw55Ex/def_face.jpg" class="img img-rounded img-fluid"/>
+        	        <p class="text-secondary text-center">15 Minutes Ago</p>
+        	    </div>
+        	    <div class="col-md-10">
+        	        <p>
+        	            <a class="float-left" href="https://maniruzzaman-akash.blogspot.com/p/contact.html"><strong>Maniruzzaman Akash</strong></a>
+        	            <span class="float-right"><i class="text-warning fa fa-star"></i></span>
+                        <span class="float-right"><i class="text-warning fa fa-star"></i></span>
+        	            <span class="float-right"><i class="text-warning fa fa-star"></i></span>
+        	            <span class="float-right"><i class="text-warning fa fa-star"></i></span>
+
+        	       </p>
+        	       <div class="clearfix"></div>
+        	        <p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        	        <p>
+        	            <a class="float-right btn btn-outline-primary ml-2"> <i class="fa fa-reply"></i> Reply</a>
+        	            <a class="float-right btn text-white btn-danger"> <i class="fa fa-heart"></i> Like</a>
+        	       </p>
+        	    </div>
+	        </div>
+	        	<div class="card card-inner">
+            	    <div class="card-body">
+            	        <div class="row">
+                    	    <div class="col-md-2">
+                    	        <img src="https://image.ibb.co/jw55Ex/def_face.jpg" class="img img-rounded img-fluid"/>
+                    	        <p class="text-secondary text-center">15 Minutes Ago</p>
+                    	    </div>
+                    	    <div class="col-md-10">
+                    	        <p><a href="https://maniruzzaman-akash.blogspot.com/p/contact.html"><strong>Maniruzzaman Akash</strong></a></p>
+                    	        <p>Lorem Ipsum is simply dummy text of the pr make  but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    	        <p>
+                    	            <a class="float-right btn btn-outline-primary ml-2">  <i class="fa fa-reply"></i> Reply</a>
+                    	            <a class="float-right btn text-white btn-danger"> <i class="fa fa-heart"></i> Like</a>
+                    	       </p>
+                    	    </div>
+            	        </div>
+            	    </div>
+	            </div>
+	    </div>
+	</div>
+
+
+ </div>
+</div> 
+
+                        
                      </div>
                   </div>
 
@@ -1489,6 +1643,14 @@ while(($rowEQ=$resEQ->fetch_assoc()) && $eqn<=4 )
       <script src="../../Resourse/js3/slick.min.js"></script> 
       <script src="../../Resourse/js3/select2.full.min.js"></script> 
       <script src="../../Resourse/js3/wow.min.js"></script> 
+
+      <script src="../../Resourse/cssSm/ratingBot/bootstrap.min.js"></script>
+<script src="../../Resourse/cssSm/ratingBot/jquery-1.11.1.min.js"></script>
+
+
+<script src="../../Resourse/cssSm/Comments/bootstrap.min.js"></script>
+
+<script src="../../Resourse/cssSm/Comments/jquery.min.js"></script>
       <!--custom js--> 
       <script src="../../Resourse/js3/custom.js"></script>
      
