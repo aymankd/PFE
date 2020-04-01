a5dda406eaf63652581360f62898ee502ba7cc9a
Kurolo <khalidbe3363@gmail.com>
2020-04-01 17:29:10 +0000

    loading equipements no icons

--- pages/samples/SeeMore.php
+++ pages/samples/SeeMore.php
@@ -1045,6 +1045,67 @@ $resEQ=$statementEQ->get_result();
 $rowEQ=$resEQ->fetch_assoc();
 
 $nbrEQ=$rowEQ['EQ'];
+
+$eqn=1;
+$equiprv1="";
+$equiprv2="";
+$equiLST="";
+$CodeE="";
+$reqEQ="SELECT * from eqlo  where CodeL=? ";
+$statementEQ=$conn->prepare($reqEQ);
+$statementEQ->bind_param("i",$CodeL);
+$statementEQ->execute();
+$resEQ=$statementEQ->get_result();
+
+while(($rowEQ=$resEQ->fetch_assoc()) && $eqn<=4 )
+ {
+   $CodeE=$rowEQ['CodeE'];
+   $reqEQS="SELECT * from equipement  where CodeE=? ";
+   $statementEQS=$conn->prepare($reqEQS);
+   $statementEQS->bind_param("i",$CodeE);
+   $statementEQS->execute();
+   $resEQS=$statementEQS->get_result();
+   $rowEQS=$resEQS->fetch_assoc();
+   if($eqn<=2)
+    {
+      $equiprv1.= " <div class='col-md-4'>
+                   <h5><svg viewBox='0 0 24 24' role='presentation' aria-hidden='true' focusable='false' style='height: 19px; width: 19px; fill: currentcolor;'><path d='m12 15a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0 5a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm5.92-5.78a.5.5 0 1 1 -.84.55c-1.19-1.81-3.07-2.77-5.08-2.77s-3.89.96-5.08 2.78a.5.5 0 0 1 -.84-.55c1.38-2.1 3.58-3.23 5.92-3.23s4.54 1.13 5.92 3.23zm2.98-3.03a.5.5 0 1 1 -.79.61c-1.66-2.14-5.22-3.8-8.11-3.8-2.83 0-6.26 1.62-8.12 3.82a.5.5 0 0 1 -.76-.65c2.05-2.42 5.75-4.17 8.88-4.17 3.19 0 7.05 1.8 8.9 4.19zm2.95-2.33a.5.5 0 0 1 -.71-.02c-2.94-3.07-6.71-4.84-11.14-4.84s-8.2 1.77-11.14 4.85a.5.5 0 0 1 -.72-.69c3.12-3.27 7.14-5.16 11.86-5.16s8.74 1.89 11.86 5.16a.5.5 0 0 1 -.02.71z' fill-rule='evenodd'></path></svg>".$rowEQS['nom']."</h5>
+                  </div>";
+      $eqn=$eqn+1;
+    }
+   else
+    {
+      $equiprv2.= " <div class='col-md-4'>
+      <h5><svg viewBox='0 0 24 24' role='presentation' aria-hidden='true' focusable='false' style='height: 19px; width: 19px; fill: currentcolor;'><path d='m12 15a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0 5a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm5.92-5.78a.5.5 0 1 1 -.84.55c-1.19-1.81-3.07-2.77-5.08-2.77s-3.89.96-5.08 2.78a.5.5 0 0 1 -.84-.55c1.38-2.1 3.58-3.23 5.92-3.23s4.54 1.13 5.92 3.23zm2.98-3.03a.5.5 0 1 1 -.79.61c-1.66-2.14-5.22-3.8-8.11-3.8-2.83 0-6.26 1.62-8.12 3.82a.5.5 0 0 1 -.76-.65c2.05-2.42 5.75-4.17 8.88-4.17 3.19 0 7.05 1.8 8.9 4.19zm2.95-2.33a.5.5 0 0 1 -.71-.02c-2.94-3.07-6.71-4.84-11.14-4.84s-8.2 1.77-11.14 4.85a.5.5 0 0 1 -.72-.69c3.12-3.27 7.14-5.16 11.86-5.16s8.74 1.89 11.86 5.16a.5.5 0 0 1 -.02.71z' fill-rule='evenodd'></path></svg>".$rowEQS['nom']."</h5>
+     </div>";
+     $eqn=$eqn+1;
+    } 
+   
+ }
+ $reqEQ2="SELECT * from eqlo  where CodeL=? ";
+ $statementEQ2=$conn->prepare($reqEQ2);
+ $statementEQ2->bind_param("i",$CodeL);
+ $statementEQ2->execute();
+ $resEQ2=$statementEQ2->get_result();
+
+ while(($rowEQ2=$resEQ2->fetch_assoc()) )
+ {
+   $CodeE=$rowEQ2['CodeE'];
+   $reqEQS="SELECT * from equipement  where CodeE=? ";
+   $statementEQS=$conn->prepare($reqEQS);
+   $statementEQS->bind_param("i",$CodeE);
+   $statementEQS->execute();
+   $resEQS=$statementEQS->get_result();
+   $rowEQS=$resEQS->fetch_assoc();
+   $nomEQ=$rowEQS['nom'];
+   
+   $equiLST.="<hr>
+              <div class='_czm8crp'>".$nomEQ."</div> 
+              <div class='_1jlnvra2'>Description d'equipement</div>";
+  
+ }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -1208,13 +1269,9 @@ $nbrEQ=$rowEQ['EQ'];
                         <div class="spe-a">
                            <h4>Équipements</h4>
                            <ul>
+                           <!--equipement preview-->
                               <li class="clearfix">
-                                 <div class="col-md-4">
-                                    <h5><svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style="height: 19px; width: 19px; fill: currentcolor;"><path d="m12 15a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0 5a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm5.92-5.78a.5.5 0 1 1 -.84.55c-1.19-1.81-3.07-2.77-5.08-2.77s-3.89.96-5.08 2.78a.5.5 0 0 1 -.84-.55c1.38-2.1 3.58-3.23 5.92-3.23s4.54 1.13 5.92 3.23zm2.98-3.03a.5.5 0 1 1 -.79.61c-1.66-2.14-5.22-3.8-8.11-3.8-2.83 0-6.26 1.62-8.12 3.82a.5.5 0 0 1 -.76-.65c2.05-2.42 5.75-4.17 8.88-4.17 3.19 0 7.05 1.8 8.9 4.19zm2.95-2.33a.5.5 0 0 1 -.71-.02c-2.94-3.07-6.71-4.84-11.14-4.84s-8.2 1.77-11.14 4.85a.5.5 0 0 1 -.72-.69c3.12-3.27 7.14-5.16 11.86-5.16s8.74 1.89 11.86 5.16a.5.5 0 0 1 -.02.71z" fill-rule="evenodd"></path></svg>     WI-FI</h5>
-                                 </div>
-                                 <div class="col-md-8">
-                                 <h5><svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style="height: 19px; width: 19px; fill: currentcolor;"><path d="m10.5 0a .5.5 0 0 0 -.5.5v7a .5.5 0 0 1 -.49.5h-1.51v-7.5a.5.5 0 1 0 -1 0v7.5h-1.51a.5.5 0 0 1 -.49-.5v-7a .5.5 0 1 0 -1 0v7c0 .83.67 1.5 1.49 1.5h1.51v5c0 .03.01.06.02.09a1.49 1.49 0 0 0 -1.02 1.41v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-7c0-.66-.43-1.21-1.02-1.41.01-.03.02-.06.02-.09v-5h1.51a1.5 1.5 0 0 0 1.49-1.5v-7a .5.5 0 0 0 -.5-.5zm-2.5 15.5v7a .5.5 0 0 1 -.5.5.5.5 0 0 1 -.5-.5v-7c0-.28.22-.5.5-.5s.5.22.5.5zm11.5-15.5h-2c-1.4 0-2.5 1.07-2.5 2.5v7c0 1.43 1.1 2.5 2.5 2.5h1.5v2.09a1.49 1.49 0 0 0 -.5-.09c-.83 0-1.5.67-1.5 1.5v7c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-22.5zm-2 11c-.86 0-1.5-.63-1.5-1.5v-7c0-.87.65-1.5 1.5-1.5h1.5v10zm1.5 11.5a.5.5 0 0 1 -.5.5.5.5 0 0 1 -.5-.5v-7c0-.28.22-.5.5-.5s.5.22.5.5z" fill-rule="evenodd"></path></svg>     Cuisine</h5>
-                                 </div>
+                                <?=$equiprv1?>
                               </li>
                               <li class="clearfix">
                                  <div class="col-md-4">
@@ -1225,19 +1282,21 @@ $nbrEQ=$rowEQ['EQ'];
                                  </div>
                               </li>
                               <li class="clearfix">
-                                 <div class="col-md-4">
-                                    <h5><svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style="height: 19px; width: 19px; fill: currentcolor;"><path d="m20 11h-19.5a.5.5 0 0 0 -.5.5v2c0 5.79 4.24 10.5 9.5 10.5 3.43 0 6.41-2.01 8.08-5h2.42a4 4 0 0 0 0-8zm-10.5 12c-4.68 0-8.5-4.24-8.5-9.5v-1.5h17v1.5c0 5.26-3.82 9.5-8.5 9.5zm10.5-5h-1.93c.59-1.37.93-2.89.93-4.5v-1.5h1a3 3 0 0 1 0 6zm-11.9-13.7c-.96-1.28-.96-2.53-.37-3.58a2.7 2.7 0 0 1 .42-.57.5.5 0 0 1 .71.71 1.73 1.73 0 0 0 -.25.35c-.41.73-.41 1.55.3 2.49 1.5 2 1.6 3.85.48 5.13a.5.5 0 1 1 -.75-.66c.79-.89.71-2.22-.53-3.87zm-4.49 1.03c-.77-.89-.77-1.85-.31-2.7a2.5 2.5 0 0 1 .32-.46.5.5 0 1 1 .74.67c-.04.04-.1.13-.18.26-.28.52-.28 1.04.15 1.54 2.12 2 2.16 3.22.45 4.29a.5.5 0 1 1 -.53-.85c1.09-.68 1.08-1.12-.64-2.75zm9 0c-.77-.89-.77-1.85-.31-2.7a2.5 2.5 0 0 1 .32-.46.5.5 0 1 1 .74.67c-.04.04-.1.13-.18.26-.28.52-.28 1.04.15 1.54 2.12 2 2.16 3.22.45 4.29a.5.5 0 0 1 -.53-.85c1.09-.68 1.08-1.12-.64-2.75z" fill-rule="evenodd"></path></svg>  Petit déjeuner</h5>
-                                 </div>
-                                 <div class="col-md-8">
-                                 <h5><svg viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false" style="height: 19px; width: 19px; fill: currentcolor;"><path d="m23.99 18.38-.5-2a .5.5 0 0 0 -.49-.38h-22a .5.5 0 0 0 -.49.38l-.5 2a .5.5 0 0 0 .49.62h23a .5.5 0 0 0 .49-.62zm-1.13-.38h-21.72l.25-1h21.22zm-21.36-3h21a .5.5 0 0 0 .5-.53c-.21-3.22-1.22-5.47-3-5.47a4911.8 4911.8 0 0 0 -8.8 0h-1.71c-.2 0-.26-.08-.19-.27a9.59 9.59 0 0 1 .17-.48c.13-.34.27-.68.43-1 .41-.79.82-1.25 1.1-1.25h10.5c.87 0 1.43-.7 1.4-1.52s-.64-1.48-1.55-1.48h-11.35c-3.84 0-7.29 4.4-8.99 11.38a.5.5 0 0 0 .49.62zm8.5-11h11.35c.35 0 .55.21.56.52.01.29-.14.48-.4.48h-10.51c-.8 0-1.42.68-1.99 1.8a10.74 10.74 0 0 0 -.65 1.61c-.31.82.23 1.59 1.13 1.59h1.71a33801.74 33801.74 0 0 1 8.8 0c .94 0 1.71 1.58 1.95 4h-19.8c1.65-6.21 4.7-10 7.85-10zm5 8a1 1 0 1 1 2 0 1 1 0 0 1 -2 0zm3 0a1 1 0 1 1 2 0 1 1 0 0 1 -2 0z" fill-rule="evenodd"></path></svg>  Fer a repasser</h5>
-
-                                 </div>
+                                 <?=$equiprv2?>
                               </li><br>
                               <div class="col-md-12">
                                     <a type="button" href="#" aria-busy="false" class="equipment" data-toggle="modal" data-target="#modalEquip">Afficher les <?=$nbrEQ?> équipements</a>
                                  </div>
                            </ul>
                         </div>
+                        <hr>
+                        <div class="spe-a">
+                           <h4>Réglements</h4>
+                           <p>
+                           <?=$regl?>
+                           </p>
+                        </div>    
+                     
                      </div>
                   </div>
 
@@ -1264,6 +1323,9 @@ $nbrEQ=$rowEQ['EQ'];
                      <a href="#" class="badge badge-primary">Contacter Hote</a>
                   </div>
                </div>
+               
+               
+               
             </div>
          </div>
       </div>
@@ -1282,35 +1344,7 @@ $nbrEQ=$rowEQ['EQ'];
       <div class="modal-body">
         <div class="_1p0spma2">Standard</div>
         <div class="_1lhxpmp">
-           <div class="_czm8crp"> Wi-Fi</div>
-           <div style="margin-top: 8px;">
-              <div class="_1jlnvra2">Accès permanent dans le logement</div> 
-              <hr>
-              <div class="_czm8crp">Television</div> 
-              <hr>
-              <div class="_czm8crp">Fer à repasser</div> 
-              <hr>
-              <div class="_czm8crp">Équipements de base</div> 
-              <div class="_1jlnvra2">Serviettes, draps, savon et papier toilette</div> 
-              <hr>
-              <div class="_czm8crp">Chauffage</div> 
-              <div class="_1jlnvra2">Chauffage central ou radiateur électrique</div> 
-              <hr>
-              <div class="_czm8crp">Climatisation</div> 
-              
-              <hr>
-              <div class="_czm8crp">Eau chaude</div> 
-              <hr>
-              <div class="_czm8crp">Detecteur de fumée</div> 
-              <hr>
-              <div class="_czm8crp">Kit de premiers secours</div> 
-              <hr>
-              <div class="_czm8crp">gg</div> 
-              <hr>
-              <div class="_czm8crp">gg</div> 
-              <hr>
-              <div class="_czm8crp">gg</div> 
-            </div>
+            <?=$equiLST?>
          </div>
          <hr>
          
@@ -1322,6 +1356,7 @@ $nbrEQ=$rowEQ['EQ'];
     </div>
   </div>
 </div>
+
 
       <footer>
          <div class="main-footer">
