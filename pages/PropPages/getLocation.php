7e57dac5859eb341a7c140e8c441326279eaac12
Nabil <bakournabilests@gmail.com>
2020-05-12 21:48:39 +0000

    getlocation page html nd css

--- pages/PropPages/getLocation.php
+++ pages/PropPages/getLocation.php
@@ -1,8 +1,5 @@
-<?php
-
-
-
-?>
+<!--
+  
 <html>
  <head>
  </head>
@@ -16,108 +13,234 @@
      <button id="cnfrm">confirmer</button>
     </div>
 
-   <script src="../../Resourse/Js/JSG/jquery.min.js"></script> 
-   
-   <script>
-   var CL=<?=$_GET['target']?>;
-   var CU=<?=$_GET['owner']?>;
-   var lng;
-   var lat;
-    $('#cl').click(function(){
-      if(navigator.geolocation)
-        {
-          navigator.geolocation.getCurrentPosition(function(position){
-           console.log(position);
-           lat=position.coords.latitude;
-            lng=position.coords.longitude;
-            
-           $.ajax({  
-                url:"isertLngLat.php?",  
-                method:"POST",  
-                data:{CodeL:CL,CodeU:CU,lng:lng,lat:lat},  
-           });
-
-          });
-        }
-      else
-        {
-          console.log("geolocation is not supported");  
-        }  
-
-    });
-   </script>   
-
- 
-<script type="text/javascript">
-        var map;
-        var clck=0;
-        var marker;
-        
-        function initMap() {                            
-            var latitude = 34.0531 ; // latitude
-            var longitude =-6.79846; //longtitude
-            
-            var myLatLng = {lat: latitude, lng: longitude};
-            
-            map = new google.maps.Map(document.getElementById('map'), {
-              center: myLatLng,
-              zoom: 8,
-              disableDoubleClickZoom: true, 
-            });
-        
-            // obtenire la positoin et placement d'un marker
-            google.maps.event.addListener(map,'dblclick',function(event) {
-
-              lat= event.latLng.lat();
-              lng =  event.latLng.lng();
-              var MLatLng={lat: lat, lng: lng};
-               if(clck==0)
-             {   marker = new google.maps.Marker({
-                  position: event.latLng, 
-                  map: map, 
-                  
-                  
-                }); clck=1;}
-                else
-                marker.setPosition(MLatLng);
-                    
-            });
-            
-           
-        }
-        </script>
-        <script >
-        $(document).ready(function(){
-           document.getElementById('mapblock').style.display='none';
-          $('#ntm').click(function () {
-        document.getElementById('mapblock').style.display='block';
-});
-
-        });
-        </script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDWv8pHQtbrov613r_RMqCjZ_nOrz2y7HM&callback=initMap"
-        async defer></script>
 
 
-        <script>   
-    $('#cnfrm').click(function(){
-     
-           $.ajax({  
-                url:"isertLngLat.php?",  
-                method:"POST",  
-                data:{CodeL:CL,CodeU:CU,lng:lng,lat:lat},  
-           });
-
-     
-    });
-   </script>
-
-<style>
-#map{
-	height:400px;
-	width:50%;
-}
-
-</style>
  </body>
+</html>
+-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   
+    <!-- base:css -->
+    <link rel="stylesheet" href="../../Resourse/LocationPage/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="../../Resourse/LocationPage/vendors/base/vendor.bundle.base.css">
+  
+    <!-- inject:css -->
+    <link rel="stylesheet" href="../../Resourse/LocationPage/css/style.css">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="../../Resourse/LocationPage/images/favicon.png" />
+  </head>
+  <body>
+    <div class="container-scroller">
+		<!-- partial:partials/_horizontal-navbar.html -->
+    <div class="horizontal-menu">
+      <nav class="navbar top-navbar col-lg-12 col-12 p-0">
+        <div class="container-fluid">
+          <div class="navbar-menu-wrapper d-flex align-items-center justify-content-between">
+            <ul class="navbar-nav navbar-nav-left">
+              <li class="nav-item ml-0 mr-5 d-lg-flex d-none">
+                <a href="#" class="nav-link horizontal-nav-left-menu"><i class="mdi mdi-format-list-bulleted"></i></a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link count-indicator dropdown-toggle d-flex align-items-center justify-content-center" id="notificationDropdown" href="#" data-toggle="dropdown">
+                  <i class="mdi mdi-bell mx-0"></i>
+                  <span class="count bg-success">2</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
+                  <p class="mb-0 font-weight-normal float-left dropdown-header">Notifications</p>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <div class="preview-icon bg-success">
+                          <i class="mdi mdi-information mx-0"></i>
+                        </div>
+                    </div>
+                    <div class="preview-item-content">
+                        <h6 class="preview-subject font-weight-normal">Application Error</h6>
+                        <p class="font-weight-light small-text mb-0 text-muted">
+                          Just now
+                        </p>
+                    </div>
+                  </a>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <div class="preview-icon bg-warning">
+                          <i class="mdi mdi-settings mx-0"></i>
+                        </div>
+                    </div>
+                    <div class="preview-item-content">
+                        <h6 class="preview-subject font-weight-normal">Settings</h6>
+                        <p class="font-weight-light small-text mb-0 text-muted">
+                          Private message
+                        </p>
+                    </div>
+                  </a>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <div class="preview-icon bg-info">
+                          <i class="mdi mdi-account-box mx-0"></i>
+                        </div>
+                    </div>
+                    <div class="preview-item-content">
+                        <h6 class="preview-subject font-weight-normal">New user registration</h6>
+                        <p class="font-weight-light small-text mb-0 text-muted">
+                          2 days ago
+                        </p>
+                    </div>
+                  </a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center" id="messageDropdown" href="#" data-toggle="dropdown">
+                  <i class="mdi mdi-email mx-0"></i>
+                  <span class="count bg-primary">4</span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
+                  <p class="mb-0 font-weight-normal float-left dropdown-header">Messages</p>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <img src="../../Resourse/images/faces/face4.jpg" alt="image" class="profile-pic">
+                    </div>
+                    <div class="preview-item-content flex-grow">
+                        <h6 class="preview-subject ellipsis font-weight-normal">David Grey
+                        </h6>
+                        <p class="font-weight-light small-text text-muted mb-0">
+                          The meeting is cancelled
+                        </p>
+                    </div>
+                  </a>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <img src="../../Resourse/images/faces/face2.jpg" alt="image" class="profile-pic">
+                    </div>
+                    <div class="preview-item-content flex-grow">
+                        <h6 class="preview-subject ellipsis font-weight-normal">Tim Cook
+                        </h6>
+                        <p class="font-weight-light small-text text-muted mb-0">
+                          New product launch
+                        </p>
+                    </div>
+                  </a>
+                  <a class="dropdown-item preview-item">
+                    <div class="preview-thumbnail">
+                        <img src="../../Resourse/images/faces/face3.jpg" alt="image" class="profile-pic">
+                    </div>
+                    <div class="preview-item-content flex-grow">
+                        <h6 class="preview-subject ellipsis font-weight-normal"> Johnson
+                        </h6>
+                        <p class="font-weight-light small-text text-muted mb-0">
+                          Upcoming board meeting
+                        </p>
+                    </div>
+                  </a>
+                </div>
+              </li>
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link count-indicator "><i class="mdi mdi-message-reply-text"></i></a>
+              </li>
+              <li class="nav-item nav-search d-none d-lg-block ml-3">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text" id="search">
+                        <i class="mdi mdi-magnify"></i>
+                      </span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="search" aria-label="search" aria-describedby="search">
+                </div>
+              </li>	
+            </ul>
+            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+                <a class="navbar-brand brand-logo" href="index.html"><img src="../../Resourse/images/logo-1.png" alt="logo"/></a>
+                <a class="navbar-brand brand-logo-mini" href="index.html"><img src="../../Resourse/images/logo-mini.svg" alt="logo"/></a>
+            </div>
+            <ul class="navbar-nav navbar-nav-right">
+               
+                
+                
+                <li class="nav-item nav-profile dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
+                    <span class="nav-profile-name">Johnson</span>
+                    <span class="online-status"></span>
+                    <img src="../../Resourse/images/faces/face28.png" alt="profile"/>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
+                      <a class="dropdown-item">
+                        <i class="mdi mdi-settings text-primary"></i>
+                        Settings
+                      </a>
+                      <a class="dropdown-item">
+                        <i class="mdi mdi-logout text-primary"></i>
+                        Logout
+                      </a>
+                  </div>
+                </li>
+            </ul>
+            <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="horizontal-menu-toggle">
+              <span class="mdi mdi-menu"></span>
+            </button>
+          </div>
+        </div>
+      </nav>
+     
+    </div>
+    <!-- partial -->
+		<div class="container-fluid page-body-wrapper">
+			<div class="main-panel">
+				<div class="content-wrapper">
+					<div class="row mt-4">
+						<div class="col-lg-8 grid-margin stretch-card">
+							<div class="card">
+								<div class="card-body">
+									
+                <h4 class="card-title">Localiser mon logement</h4>
+                <hr>
+											<div class="row">
+                      <button type="button" class="btn btn-primary centredZero">Naviguer sur la carte</button>
+                      </br>     
+                      <button type="button" class="btn btn-primary centredOne">Me localiser</button>
+                      </div>
+</br>
+                      <h4 class="card-title">Merci de confirmer la localisation choisie</h4>
+                <hr>
+
+                <button type="button" class="btn btn-success">Confirmer</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					
+					
+				</div>
+				<!-- content-wrapper ends -->
+				<!-- partial:partials/_footer.html -->
+				
+				<!-- partial -->
+			</div>
+			<!-- main-panel ends -->
+		</div>
+		<!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- base:js -->
+    <script src="../../Resourse/LocationPage/vendors/base/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="../../Resourse/LocationPage/js/template.js"></script>
+    <!-- endinject -->
+    <!-- plugin js for this page -->
+    <!-- End plugin js for this page -->
+  
+    <!-- Custom js for this page-->
+    <script src="../../Resourse/LocationPage/js/dashboard.js"></script>
+    <!-- End custom js for this page-->
+  </body>
 </html>
